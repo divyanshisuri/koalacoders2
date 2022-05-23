cb21d988044a3955eca427f741070425dcff44fa
divyanshisuri <divyanshi.suri@gmail.com>
2022-05-23 12:21:59 -0700

    edits

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def grocerylist():
 def spinningwheel():
     return render_template("spinningwheel.html")
 
+@app.route('/gradecalc/')
+def gradecalc():
+    return render_template("gradecalc.html")
+
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
